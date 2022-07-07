d552d1a8447c28af90c7b65c99f5917c128f66fe
Giga Chubinidze <gchubinidze@unisens.ge>
2022-07-07 17:46:33 +0400

    started carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy 
   has_many :comments, dependent: :destroy
+  mount_uploader :avatar, AvatarUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
